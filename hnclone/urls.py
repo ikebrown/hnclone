a2bb3d889e9b7eacd451339bbe9953000407eb9b
Andre Lashley <andre.lashley@gmail.com>
2013-06-03 21:58:03 +0000

    Initial commit

---
+++ hnclone/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('stories.urls')),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth/login.html'}, name='login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+)
